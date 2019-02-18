2226fd194919f3fd404481619cb244b2244f5750
VIMGAS <victormoraesgs@hotmail.com>
2019-02-18 15:15:06 -0300

    Initial tests

---
+++ tests/middlewares/requestIndentifier.test.js
@@ -0,0 +1,52 @@
+var request = require('supertest');
+var App = require('./../../src/app.js');
+
+describe('Request Identifier Middleware', () => {
+  it('Should add the provided RequestID to the Response', async (done) => {
+    var application = new App({
+      enableLogger: false,
+      enableRequestIdentifier: true
+    });
+    const response = await request(application)
+      .get('/status')
+      .set('RequestID', '834180130-ioasdsaj-12381231');
+
+    expect(response.headers.requestid).toBe('834180130-ioasdsaj-12381231');
+    done();
+  });
+
+  it('Should add a new RequestID to the Response', async (done) => {
+    var application = new App({
+      enableLogger: false,
+      enableRequestIdentifier: true
+    });
+    const response = await request(application).get('/status');
+
+    expect(typeof response.headers.requestid).toBe('string');
+    done();
+  });
+
+  it('Should not add a new RequestID to the Response', async (done) => {
+    var application = new App({
+      enableLogger: false,
+      enableRequestIdentifier: false
+    });
+    const response = await request(application).get('/status');
+
+    expect(typeof response.headers.requestid).toBe('undefined');
+    done();
+  });
+
+  it('Should not propagate the specified RequestID to the Response', async (done) => {
+    var application = new App({
+      enableLogger: false,
+      enableRequestIdentifier: false
+    });
+    const response = await request(application)
+      .get('/status')
+      .set('RequestID', '834180130-ioasdsaj-12381231');
+
+    expect(typeof response.headers.requestid).toBe('undefined');
+    done();
+  });
+});
---
+++ tests/middlewares/workerIdentifier.test.js
@@ -0,0 +1,26 @@
+var request = require('supertest');
+var App = require('./../../src/app.js');
+
+describe('Worker Identifier Middleware', () => {
+  it('Should add the workerid 1 to the Response', async (done) => {
+    var application = new App({
+      enableLogger: false,
+      mode: 'worker'
+    });
+    const response = await request(application).get('/status');
+
+    expect(response.headers.workerid).toBe('1');
+    done();
+  });
+
+  it('Should not add a workerid to the Response', async (done) => {
+    var application = new App({
+      enableLogger: false,
+      mode: 'main'
+    });
+    const response = await request(application).get('/status');
+
+    expect(typeof response.headers.workerid).toBe('undefined');
+    done();
+  });
+});
