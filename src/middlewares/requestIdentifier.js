519dbbde6f6aa6fb11dccb78d30397e3e29fccc9
VIMGAS <victormoraesgs@hotmail.com>
2019-02-18 13:13:54 -0300

    @param comment fix

--- src/middlewares/requestIdentifier.js
+++ src/middlewares/requestIdentifier.js
@@ -5,12 +5,13 @@ import uuid from 'uuid/v4';
  * @param {Express.Request} req Express Request Object
  * @param {string|null} req.headers.RequestID The Predefined Request Identifier
  * @param {Express.Response} res Express Response Object
- * @param {Express.Next} next Express Next callback for middlewares
+ * @param {Function} next Express Next callback for middlewares
  * @function requestIdentifier
  * @middleware
  */
 function requestIdentifier(req, res, next) {
   res.header('RequestID', req.get('RequestID') || uuid());
+  req.identifier = res.get('RequestID');
   return next();
 }
 
