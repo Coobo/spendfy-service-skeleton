8e97eac9de568e2ca4d76a46cc3d794107952226
vimgas <victormoraesgs@hotmail.com>
2019-08-01 12:07:46 -0300

    Identation Update on Tests

--- tests/middlewares/workerIdentifier.test.js
+++ tests/middlewares/workerIdentifier.test.js
@@ -2,25 +2,25 @@ var request = require('supertest');
 var App = require('./../../src/app.js');
 
 describe('Worker Identifier Middleware', () => {
-  it('Should add the workerid 1 to the Response', async (done) => {
-    var application = new App({
-      enableLogger: false,
-      mode: 'worker'
+    it('Should add the workerid 1 to the Response', async (done) => {
+        var application = new App({
+            enableLogger: false,
+            mode: 'worker'
+        });
+        const response = await request(application).get('/status');
+
+        expect(response.headers.workerid).toBe('1');
+        done();
     });
-    const response = await request(application).get('/status');
 
-    expect(response.headers.workerid).toBe('1');
-    done();
-  });
+    it('Should not add a workerid to the Response', async (done) => {
+        var application = new App({
+            enableLogger: false,
+            mode: 'main'
+        });
+        const response = await request(application).get('/status');
 
-  it('Should not add a workerid to the Response', async (done) => {
-    var application = new App({
-      enableLogger: false,
-      mode: 'main'
+        expect(typeof response.headers.workerid).toBe('undefined');
+        done();
     });
-    const response = await request(application).get('/status');
-
-    expect(typeof response.headers.workerid).toBe('undefined');
-    done();
-  });
 });
