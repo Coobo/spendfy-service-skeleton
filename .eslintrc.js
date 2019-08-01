31c146f8111f42e93066a2360782be8195f51792
vimgas <victormoraesgs@hotmail.com>
2019-08-01 12:01:32 -0300

    Updating tabSize to 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-    jest: true
-  },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  rules: {
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'no-console': 0
-  }
+    env: {
+        es6: true,
+        node: true,
+        jest: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-console': 0
+    }
 };
