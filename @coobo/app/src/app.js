215ca6df62834f5a2b2a728efbd889195ecbaa82
VIMGAS <victormoraesgs@gmail.com>
2019-10-13 14:37:59 -0300

    @coobo/app initial setup

---
+++ @coobo/app/src/app.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+class Application {
+  constructor({ appRoot }) {
+    this.inProduction = process.env.NODE_ENV === 'production';
+    this.inDev = !this.inProduction;
+
+    this.appRoot = appRoot;
+    this.appName = process.env.APP_NAME || 'spendfy-service';
+    this.package = require(path.join(appRoot, 'package.json'));
+    this.version = this.package.version || '0.0.0';
+    this.coreVersion = this.package.dependencies['@coobo/app'];
+  }
+
+  makePath(...paths) {
+    return path.join(this.appRoot, ...paths);
+  }
+
+  configPath() {
+    return path.join(this.appRoot, 'config');
+  }
+
+  servicesPath() {
+    return path.join(this.appRoot, 'services');
+  }
+
+  domainsPath() {
+    return path.join(this.appRoot, 'domains');
+  }
+
+  controllersTest() {
+    return /(.*)Controller\.js/i;
+  }
+
+  modelsTest() {
+    return /(.*)Model\.js/i;
+  }
+
+  validatorsTest() {
+    return /(.*)Validator\.js/i;
+  }
+
+  seedersTest() {
+    return /(.*)Seeder\.js/i;
+  }
+}
+
+module.exports = Application;
